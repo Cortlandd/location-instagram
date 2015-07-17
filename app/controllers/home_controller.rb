078303cfef3058ad8eb33efcb14f4495bed3f190
Cortland Walker <cortland1568@gmail.com>
2015-07-17 12:21:06 -0400

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   	def index
-   	access_token = "1615794409.84795f1.4fb516b7372a4664bd7b18d052968231" # should have been session[:access_token]. Never store credentials in your code
+   	access_token = "1615794409.84795f1.4fb516b7372a4664bd7b18d052968231" # should have been session[:access_token]. Never store credentials in the code
    	client = Instagram.client(access_token: access_token)
 
    	geocoder = Geocoder.search(params[:q] || "chicago, illinois")
